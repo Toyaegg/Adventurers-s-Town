e5d4dd499e67fa370a860716a0172e0d758ccc0d
Toyaegg <58324967+Toyaegg@users.noreply.github.com>
2024-08-05 16:17:15 +0800

    22

--- scripts/game/systems/adventurer_system/adventurer/exp.gd
+++ scripts/game/systems/adventurer_system/adventurer/exp.gd
@@ -1,7 +1,30 @@
 class_name EXP
 extends Node
 
+const MAX_EXP_VALUE = 100000
+
 var level : int
 var cur_exp : float
 var exp_multiply : float
 @export var exp_multiply_factor : float = 2
+@export var curve : Curve
+
+signal level_up(level : int)
+
+var max_exp : int:
+	get:
+		return MAX_EXP_VALUE * curve.sample(level / Adventurer.MAX_LEVEL)
+
+
+var exp_amount : float:
+	get:
+		return float(cur_exp) / max_exp
+		
+
+func add_exp(value : int) -> void:
+	cur_exp += value
+	var max = max_exp
+	if cur_exp >= max:
+		cur_exp -= max
+		level += 1
+		level_up.emit(level)
--- scripts/game/systems/adventurer_system/adventurer/mp.gd
+++ scripts/game/systems/adventurer_system/adventurer/mp.gd
@@ -1,10 +1,27 @@
 class_name MP
 extends Node
 
-var mp : float
+const MAX_MP = 100
 
+#var mp : float
 var cur_mp : float
+
+@export var mp_restore : float = 20.0
 
 var mp_amount : float:
 	get:
-		return cur_mp / mp;
+		return cur_mp / MAX_MP;
+
+var cur_day : int
+
+func _ready() -> void:
+	EventBus.subscribe(GameEvents.TIME_VALUE_CHANGED, time_changed)
+
+
+func time_changed(time : TimeSystem.TimeData) -> void:
+	if cur_day != time.day:
+		print("mp 恢复 20 点")
+		cur_day = time.day
+		cur_mp += mp_restore
+		if cur_mp > MAX_MP:
+			cur_mp = MAX_MP
